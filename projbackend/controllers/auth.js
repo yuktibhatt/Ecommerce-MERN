a28b2e85e65056a0674c7db706c05696db997e75
yuktibhatt <bhattyukti5@gmail.com>
2021-05-30 18:16:26 +0545

    singup routes

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,3 +1,9 @@
+exports.signup = (req, res) => {
+    res.json({
+        message: "Signup works"
+    });
+};
+
 exports.signout = (req, res) => {
     res.json({
         message : "User Signout Success"
