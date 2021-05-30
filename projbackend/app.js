a9c75d962ae9dc933e979eb9e9691efd854eeded
yuktibhatt <bhattyukti5@gmail.com>
2021-05-30 10:09:23 +0545

    routers

--- projbackend/app.js
+++ projbackend/app.js
@@ -3,7 +3,13 @@ require("dotenv").config();
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
 
+
+//db connections
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology : true,
@@ -13,9 +19,18 @@ mongoose.connect(process.env.DATABASE, {
     console.log("DB CONNECTED");
 });
 
+//middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
+//routes
+app.use("/api", authRoutes);
+
+//ports
 const port = process.env.PORT || 8000;
 
+//starting a server
 app.listen(port, () => {
     console.log(`app is running at ${port}`);
 });
